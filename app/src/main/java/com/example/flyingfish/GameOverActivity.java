31af5e122460f03adb1503f96bc59a151c9d69b0
Eric Florence <ericflorence.dev@gmail.com>
2019-05-10 16:41:03 -0700

    FLYING FISH

---
+++ app/src/main/java/com/example/flyingfish/GameOverActivity.java
@@ -0,0 +1,36 @@
+package com.example.flyingfish;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    private Button restartGame;
+    private TextView displayScore;
+    private String score;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        score = getIntent().getExtras().get("score").toString();
+
+        restartGame = (Button) findViewById(R.id.play_again);
+        displayScore = (TextView) findViewById(R.id.displayScore);
+
+        restartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(GameOverActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
+        displayScore.setText("Score = " + score);
+    }
+}
